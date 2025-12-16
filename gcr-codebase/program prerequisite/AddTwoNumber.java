b6e4fa21993a42c964de4edc35d0b0d1c7bff23e
vanshu0525 <vanshslathia03@gmail.com>
2025-12-16 16:59:17 +0530

    [Vansh] : added program to add numbers

---
+++ gcr-codebase/program prerequisite/AddTwoNumber.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class AddTwoNumber{
+public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter First Number");
+	int a = sc.nextInt();
+	System.out.println("Enter Second Number");
+	int b = sc.nextInt();
+	int ans = a+b;
+	System.out.println("Result : " + ans);
+}
+}
