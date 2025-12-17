942cbbbe3d11c03668fe8c6b1d93ca0b771a2c39
vanshu0525 <vanshslathia03@gmail.com>
2025-12-17 14:55:42 +0530

    [Vansh] : added program to convert from Km to Miles

---
+++ gcr-codebase/programming-elements/Level1/KmToMiles.java
@@ -0,0 +1,7 @@
+public class KmToMiles{
+public static void main(String[] args){
+	float km = (float) 10.8;
+	float miles =(float)( km * 1.6);
+ 	System.out.println("The distance " + km + " km in miles is " + 	miles);
+}
+}
