95192730ce4cc6f8be18f713c62fdbfc6b398cb4
vanshu0525 <vanshslathia03@gmail.com>
2025-12-17 16:34:01 +0530

    [Vansh] : modified program to calculate Profit

--- gcr-codebase/programming-elements/Level1/ProfitCalculation.java
+++ gcr-codebase/programming-elements/Level1/ProfitCalculation.java
@@ -1,11 +1,14 @@
 public class ProfitCalculation {
     public static void main(String[] args) {
+	
         double costPrice = 129;
         double sellingPrice = 191;
-
+	//Calculating Profit
         double profit = sellingPrice - costPrice;
+	//Calculating Profit Percentage
         double profitPercentage = (profit / costPrice) * 100;
-
+	
+	//Printing Cost Price,Selling price, Profit and Profit Percentage
         System.out.println(
                 "The Cost Price is INR " + costPrice + " and Selling Price is INR " + sellingPrice + "\n" +
                 "The Profit is INR " + profit + " and the Profit Percentage is " + profitPercentage
