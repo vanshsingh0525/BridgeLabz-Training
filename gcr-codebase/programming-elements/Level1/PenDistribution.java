83c66d1331c900af6c8230dd14d1361b836c0603
vanshu0525 <vanshslathia03@gmail.com>
2025-12-17 16:39:10 +0530

    [Vansh] : modified program to calculate Pen distribution

--- gcr-codebase/programming-elements/Level1/PenDistribution.java
+++ gcr-codebase/programming-elements/Level1/PenDistribution.java
@@ -1,11 +1,14 @@
 public class PenDistribution {
     public static void main(String[] args) {
+
         int totalPens = 14;
         int students = 3;
-
+	//Calculating pens per Student
         int pensPerStudent = totalPens / students;
+	//Calculating remaining pens
         int remainingPens = totalPens % students;
 
+	//Printing pens per Student and remaining pens
         System.out.println(
                 "The Pen Per Student is " + pensPerStudent +
                 " and the remaining pen not distributed is " + remainingPens
