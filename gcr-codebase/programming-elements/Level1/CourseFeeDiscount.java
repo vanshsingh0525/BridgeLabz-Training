2df6537eae56cd07ceafae129aaa8dff6b10d0b3
vanshu0525 <vanshslathia03@gmail.com>
2025-12-17 16:42:34 +0530

    [Vansh] : modified program to find discount for course fee

--- gcr-codebase/programming-elements/Level1/CourseFeeDiscount.java
+++ gcr-codebase/programming-elements/Level1/CourseFeeDiscount.java
@@ -1,11 +1,14 @@
 public class CourseFeeDiscount {
     public static void main(String[] args) {
+
         double fee = 125000;
         double discountPercent = 10;
-
+	//Calculating discount
         double discount = (fee * discountPercent) / 100;
+	//Calculating final fee
         double finalFee = fee - discount;
 
+	//Printing discount and final fee
         System.out.println(
                 "The discount amount is INR " + discount +
                 " and final discounted fee is INR " + finalFee
