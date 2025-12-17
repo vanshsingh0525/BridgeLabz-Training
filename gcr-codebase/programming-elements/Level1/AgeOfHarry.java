7ab310425105474d310f47c44051fc2f20e23c7f
vanshu0525 <vanshslathia03@gmail.com>
2025-12-17 16:07:13 +0530

    [Vansh] : modified program to find age of Harry

--- gcr-codebase/programming-elements/Level1/AgeOfHarry.java
+++ gcr-codebase/programming-elements/Level1/AgeOfHarry.java
@@ -1,10 +1,13 @@
 public class AgeOfHarry {
     public static void main(String[] args) {
+
+	
         int birthYear = 2000;
         int currentYear = 2024;
-
+	//Calculating current age
         int age = currentYear - birthYear;
 
+	//Printing current age
         System.out.println("Harry's age in 2024 is " + age);
     }
 }
