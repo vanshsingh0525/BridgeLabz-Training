32850c8178d59e297a99a7d7ad94d387500ee9cc
vanshu0525 <vanshslathia03@gmail.com>
2025-12-19 15:52:45 +0530

    [Vansh] : added program to find Total Sum of values in Arrays

---
+++ gcr-codebase/arrays/Level1/ArrayValues.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class ArrayValues{
+    public static void main(String[] args){
+	Scanner s = new Scanner(System.in);
+
+	//Initialize the array
+	double arr[] = new double[10];
+	double total = 0.0;
+	int index = 0;
+	while(true){
+	     double num = s.nextDouble();
+	     if(num <=0 || index == 10){
+		break;
+	     }
+	     arr[index] = num;
+	     index++;
+	     
+	}
+	for(int i=0; i<arr.length; i++){
+	     total += arr[i];
+	}
+
+	//Printing the total sum
+	System.out.println(total);
+    }
+}
