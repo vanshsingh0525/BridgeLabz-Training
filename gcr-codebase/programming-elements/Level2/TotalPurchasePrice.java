fc3ce352bb220f668eabd5a0760cf68acf30b813
vanshu0525 <vanshslathia03@gmail.com>
2025-12-21 23:21:47 +0530

    [Vansh] : added program for Total Purchase Price

---
+++ gcr-codebase/programming-elements/Level2/TotalPurchasePrice.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TotalPurchasePrice {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Taking input
+        System.out.print("Enter the unit price: ");
+        double unitPrice = input.nextDouble();
+        System.out.print("Enter the quantity: ");
+        int quantity = input.nextInt();
+        // Calculating total price
+        double totalPrice = unitPrice * quantity;
+
+        // Display the output
+        System.out.println("The total purchase price is INR " + totalPrice + " if the quantity " + quantity + " and unit price is INR " + unitPrice);
+    }
+}
