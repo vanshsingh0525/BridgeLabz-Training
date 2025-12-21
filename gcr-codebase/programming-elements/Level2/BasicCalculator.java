d8c75756d2697fe5dd246a921f7776653ba17018
vanshu0525 <vanshslathia03@gmail.com>
2025-12-21 22:47:41 +0530

    [Vansh] : added program for basic Calculator

---
+++ gcr-codebase/programming-elements/Level2/BasicCalculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Taking input
+        System.out.print("Enter the first number: ");
+        double number1 = input.nextDouble();
+        System.out.print("Enter the second number: ");
+        double number2 = input.nextDouble();
+
+        // Performing arithmetic operations
+        double addition = number1 + number2;
+        double subtraction = number1 - number2;
+        double multiplication = number1 * number2;
+        double division = number1 / number2;
+
+        // Display the output
+        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers " + number1 + " and " + number2 + " is " + addition + ", " + subtraction + ", " + 	multiplication + ", and " + division);
+    }
+}
