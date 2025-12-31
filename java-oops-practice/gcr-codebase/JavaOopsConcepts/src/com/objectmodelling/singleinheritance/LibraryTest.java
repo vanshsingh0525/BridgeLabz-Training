baa99f30b22c8d817a5d27b34bdddb981bb47550
vanshu0525 <vanshslathia03@gmail.com>
2025-12-31 15:09:09 +0530

    [Vansh] : added program for Library Details

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/singleinheritance/LibraryTest.java
@@ -0,0 +1,18 @@
+package com.objectmodelling.singleinheritance;
+
+public class LibraryTest {
+
+    public static void main(String[] args) {
+
+        Author bookWithAuthor = new Author(
+                "Effective Java",
+                2018,
+                "Joshua Bloch",
+                "Expert in Java programming and API design"
+        );
+
+        bookWithAuthor.displayInfo();
+    }
+}
+
+
