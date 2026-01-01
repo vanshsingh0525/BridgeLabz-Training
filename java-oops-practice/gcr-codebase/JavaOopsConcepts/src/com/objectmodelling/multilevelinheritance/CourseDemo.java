194a44e9755c5904f54d5b0f62f74980d2fd9c81
vanshu0525 <vanshslathia03@gmail.com>
2026-01-01 11:49:05 +0530

    [Vansh] : added program for Education Course

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/multilevelinheritance/CourseDemo.java
@@ -0,0 +1,15 @@
+package com.objectmodelling.multilevelinheritance;
+
+public class CourseDemo {
+
+    public static void main(String[] args) {
+
+        PaidOnlineCourse course =
+            new PaidOnlineCourse("Java Full Stack", 60,
+                                 "Udemy", true,
+                                 10000, 20);
+
+        course.displayPaidCourseDetails();
+    }
+}
+
