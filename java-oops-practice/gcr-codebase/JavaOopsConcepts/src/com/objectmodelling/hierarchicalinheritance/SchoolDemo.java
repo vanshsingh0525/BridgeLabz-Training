a561e30a15f7bce19d3dc5d0da5027cbc58cdb7e
vanshu0525 <vanshslathia03@gmail.com>
2026-01-01 12:41:14 +0530

    [Vansh] : added program for School details

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/hierarchicalinheritance/SchoolDemo.java
@@ -0,0 +1,28 @@
+package com.objectmodelling.hierarchicalinheritance;
+
+public class SchoolDemo {
+
+    public static void main(String[] args) {
+
+        Teacher t =
+            new Teacher("Mr. Sharma", 40, "Mathematics");
+
+        Student s =
+            new Student("Anita", 16, "10th Grade");
+
+        Staff st =
+            new Staff("Ramesh", 45, "Administration");
+
+        t.displayPersonDetails();
+        t.displayRole();
+        System.out.println();
+
+        s.displayPersonDetails();
+        s.displayRole();
+        System.out.println();
+
+        st.displayPersonDetails();
+        st.displayRole();
+    }
+}
+
