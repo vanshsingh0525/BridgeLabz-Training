91f70b20405fb96361452044429af6e51b0fb25a
vanshu0525 <vanshslathia03@gmail.com>
2026-01-26 10:30:00 +0530

    [Vansh] : added Json validate program

---
+++ java-iostreams-practice/gcr-codebase/JavaIOStreamsConcepts/src/json/jsonvalidate/JsonValidate.java
@@ -0,0 +1,25 @@
+package json.jsonvalidate;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+
+public class JsonValidate {
+    public static void main(String[] args) {
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode node = mapper.readTree(new File("src/json/jsonvalidate/user.json"));
+
+            if (node.has("name") && node.has("email")) {
+                System.out.println("JSON is valid");
+            } else {
+                System.out.println("JSON is invalid");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
