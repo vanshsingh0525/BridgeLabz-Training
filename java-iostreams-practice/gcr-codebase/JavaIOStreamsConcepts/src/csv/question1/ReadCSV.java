02f7a648927e622846909c5ab4267e9adfb5149d
vanshu0525 <vanshslathia03@gmail.com>
2026-01-27 23:36:27 +0530

    [Vansh] : added program for Reading a CSV File and Print Data

---
+++ java-iostreams-practice/gcr-codebase/JavaIOStreamsConcepts/src/csv/question1/ReadCSV.java
@@ -0,0 +1,21 @@
+package csv.question1;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadCSV {
+    public static void main(String[] args) {
+        String file = "C:\\Users\\hp\\Desktop\\BridgeLabz-Training\\java-iostreams-practice\\gcr-codebase\\JavaIOStreamsConcepts\\src\\csv\\question1\\students.csv";
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String header = br.readLine(); // Skip header
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                System.out.println("ID: " + data[0] + ", Name: " + data[1] +
+                                   ", Age: " + data[2] + ", Marks: " + data[3]);
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
