eb527f70283b3d3445578f78b19f3f3007519d5b
vanshu0525 <vanshslathia03@gmail.com>
2026-01-27 23:38:29 +0530

    [Vansh] : added program for Writing Data to a CSV File

---
+++ java-iostreams-practice/gcr-codebase/JavaIOStreamsConcepts/src/csv/question2/WriteCSV.java
@@ -0,0 +1,22 @@
+package csv.question2;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteCSV {
+    public static void main(String[] args) {
+        String file = "C:\\Users\\hp\\Desktop\\BridgeLabz-Training\\java-iostreams-practice\\gcr-codebase\\JavaIOStreamsConcepts\\src\\csv\\question2\\employees.csv";
+        String data = "ID,Name,Department,Salary\n" +
+                      "101,John,IT,50000\n" +
+                      "102,Alice,HR,45000\n" +
+                      "103,Bob,Finance,48000\n" +
+                      "104,Emma,IT,52000\n" +
+                      "105,David,Marketing,47000\n";
+
+        try (FileWriter fw = new FileWriter(file)) {
+            fw.write(data);
+            System.out.println("CSV file created successfully!");
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
