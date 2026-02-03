a390a235a1f54c363f8e647b28cbd038fefb0634
vanshu0525 <vanshslathia03@gmail.com>
2026-02-03 15:41:42 +0530

    [Vansh] :Added ExamResultUploader – Bulk Marks Processing

---
+++ java-collections-practice/scenario-based-codebase/ScenarioBasedQuestions/src/examresultuploader/InvalidMarkFormatException.java
@@ -0,0 +1,7 @@
+package examresultuploader;
+
+public class InvalidMarkFormatException extends Exception {
+    public InvalidMarkFormatException(String message) {
+        super(message);
+    }
+}
