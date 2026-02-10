a96573fd66ba4c72fffdc0c39f70abf9bd3481e8
vanshu0525 <vanshslathia03@gmail.com>
2026-02-10 19:36:12 +0530

    [Vansh] : modified program to reverse a queue

--- java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/collections/queue/ReverseQueue.java
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/collections/queue/ReverseQueue.java
@@ -1,5 +1,40 @@
 package collections.queue;
 
+import java.util.*;
+
 public class ReverseQueue {
 
+    // Reverse queue using recursion and only queue operations
+    public static void reverseQueue(Queue<Integer> queue) {
+
+        if (queue.isEmpty()) {
+            return;
+        }
+
+        int front = queue.remove();   // remove front
+        reverseQueue(queue);          // reverse remaining queue
+        queue.add(front);             // add element at rear
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> queue = new LinkedList<>();
+
+        System.out.print("Enter number of elements in queue: ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter queue elements:");
+        for (int i = 0; i < n; i++) {
+            queue.add(sc.nextInt());
+        }
+
+        System.out.println("Original Queue: " + queue);
+
+        reverseQueue(queue);
+
+        System.out.println("Reversed Queue: " + queue);
+
+        sc.close();
+    }
 }
